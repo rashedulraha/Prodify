cee37bb122e1a34ec7f7ac1f7e0328d7668092e4
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-11-23 01:18:22 +0600

    impliment login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,21 @@
+// !This is a server component by default
+import LoginForm from "@/app/_components/LoginForm";
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="max-w-sm mx-auto bg-white p-8 rounded-lg shadow-md ">
+      <LoginForm />
+
+      <div className="flex items-center justify-center mt-5 text-sm gap-5">
+        <p>Don&apos;t Have an account ? </p>
+        <Link className="text-blue-500" href={"/register"}>
+          Register
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ src/app/_components/LoginForm.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <form className="space-y-3">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+          className="mt-1 block rounded-sm border border-blue-600 focus:outline-2 outline-blue-800 outline-offset-1 w-full py-1 px-3  text-sm "
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your valid password"
+          required
+          className="mt-1 block rounded-sm border border-blue-600 focus:outline-2 outline-blue-800 outline-offset-1 w-full py-1 px-3  text-sm "
+        />
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
